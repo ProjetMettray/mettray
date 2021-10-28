6539c5ad39a5336d58848bdabd5cdd73bab2a799
Kevin BERTAUX <kevinbertaux@gmail.com>
2021-10-28 09:21:41 +0200

    [LOCATION][WIP] début du controller

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,12 +12,32 @@ class LocationController extends AbstractController
     /**
      * @Route("/location", name="location")
      */
-    public function index(): Response
+    public function index(LocationRepository $locationRepository): Response
     {
         return $this->render('location/index.html.twig', [
-            'controller_name' => 'LocationController',
+            'location' => $locationRepository->findAll(),
         ]);
     }
 
-    
+    public function addLocation(Request $request): Response
+    {
+        $location = $this->getLocation();
+        
+        $location = new Location();
+        $form =$this->createForm(LocationType::class, $location);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute()
+        }
+
+        return $this->renderForm('location/index.html.twig', [
+            'location' => $location,
+            'form' => $form,
+        ]);
+    }
 }
