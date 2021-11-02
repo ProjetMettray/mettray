1e359c60ae739807db3ff75e9a09d5c293975e52
Cauvin <adrien.cauvin@gmail.com>
2021-11-02 11:37:07 +0100

    Moving ReservationByUser to a special file before merge

---
+++ ReservationByUser/Controller/BookingController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Booking;
+use App\Form\BookingType;
+use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class BookingController extends AbstractController
+{
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/booking", name="booking")
+     */
+    public function index(BookingRepository $BookingRepository): Response
+    {
+        return $this->render('booking/index.html.twig', [
+            'bookings' => $BookingRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/booking/show_all_by_user", name="booking_show_all_by_user")
+     */
+    public function showAllByUser(): Response
+    {
+        $userId = $this->getUser()->getId();
+        $bookings = $this->em->getRepository(Booking::class)->findByUserId($userId);
+        return $this->render('booking/show_all_by_user.html.twig', [
+            'bookings' => $bookings
+        ]);
+    }
+
+    /**
+     * @Route("/booking/new", name="booking_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class, $booking);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($booking);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('booking_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('booking/new.html.twig', [
+            'booking' => $booking,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/booking/{id}", name="booking_show", methods={"GET"})
+     */
+    public function show(Booking $booking): Response
+    {
+        return $this->render('booking/show.html.twig', [
+            'booking' => $booking,
+        ]);
+    }
+
+    /**
+     * @Route("/booking/{id}/edit", name="booking_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Booking $booking): Response
+    {
+        $form = $this->createForm(BookingType::class, $booking);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('booking_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('booking/edit.html.twig', [
+            'booking' => $booking,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/booking/{id}", name="booking_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Booking $booking): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$booking->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($booking);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('booking_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
