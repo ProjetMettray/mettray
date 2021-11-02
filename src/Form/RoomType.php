d7548d699d5e4be14d8e730b9d9c83aaccb16ac5
DimitriGLLN <dimitri.guillon331@gmail.com>
2021-11-02 09:44:58 +0100

    [FEATURE][ROOM] début de la mise en place du style

--- src/Form/RoomType.php
+++ src/Form/RoomType.php
@@ -3,21 +3,49 @@
 namespace App\Form;
 
 use App\Entity\Room;
+use Doctrine\DBAL\Types\ArrayType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class RoomType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('nbPlace')
-            ->add('description')
-            ->add('location')
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control input-form'
+                ],
+            ])
+
+            ->add('nbPlace',NumberType::class, [
+                'label' => 'Nombre de place',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control input-form'
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control input-form'
+                ],
+            ])
+            ->add('location', ChoiceType::class, [
+                'group_by' => ChoiceList::groupBy($this, 'name'),
+            ])
             //->add('room_has_user')
             ->add('room')
+            ->add('submit', SubmitType::class)
         ;
     }
 
