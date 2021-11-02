f8b743fad9b52aa69aadaadb3235104dc4b02cc5
Alan <daunay123@gmail.com>
2021-11-02 11:56:54 +0100

    [FEATURE] finalisation du form roomType

--- src/Form/RoomType.php
+++ src/Form/RoomType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Room;
+use App\Entity\Location;
 use Doctrine\DBAL\Types\ArrayType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -41,10 +43,26 @@ class RoomType extends AbstractType
                     'class' => 'form-control input-form'
                 ],
             ])
-            ->add('location')
+            ->add('location', EntityType::class, [
+                'class' => Location::class,
+                'label' => 'Location',
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false
+            ])
             //->add('room_has_user')
-            ->add('room')
-            ->add('submit', SubmitType::class);
+            ->add('room', EntityType::class, [
+                'class' => Room::class,
+                'label' => 'Room',
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
