2ce7fa147da73b8a3357086756b9d17d4752bd25
Alan <daunay123@gmail.com>
2021-11-02 11:46:23 +0100

    [FEATURE] roomType modif

--- src/Form/RoomType.php
+++ src/Form/RoomType.php
@@ -5,12 +5,13 @@ namespace App\Form;
 use App\Entity\Room;
 use Doctrine\DBAL\Types\ArrayType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 
 class RoomType extends AbstractType
@@ -26,7 +27,7 @@ class RoomType extends AbstractType
                 ],
             ])
 
-            ->add('nbPlace',NumberType::class, [
+            ->add('nbPlace', NumberType::class, [
                 'label' => 'Nombre de place',
                 'required' => true,
                 'attr' => [
@@ -40,13 +41,10 @@ class RoomType extends AbstractType
                     'class' => 'form-control input-form'
                 ],
             ])
-            ->add('location', ChoiceType::class, [
-                'group_by' => ChoiceList::groupBy($this, 'name'),
-            ])
+            ->add('location')
             //->add('room_has_user')
             ->add('room')
-            ->add('submit', SubmitType::class)
-        ;
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
