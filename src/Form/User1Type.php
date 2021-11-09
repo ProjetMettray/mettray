5a6ca1d99031392c7e7875681c7771b61d74606a
Alan <daunay123@gmail.com>
2021-11-09 10:14:07 +0100

    [FEATURE] AJout du form modifiez profil

--- src/Form/User1Type.php
+++ src/Form/User1Type.php
@@ -6,19 +6,42 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class User1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('phone')
-        ;
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control input-form'
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control input-form'
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control input-form'
+                ],
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control input-form'
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
